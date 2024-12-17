efd00727f850ed7ceadbe5843fd01b684767404b
ShoopDeWoop <116699852+Rikuron@users.noreply.github.com>
2024-12-17 14:31:57 +0800

    Final Update (check for revisions)
    Only need pictures from Sir William to replace for Services.

--- js/scripts.js
+++ js/scripts.js
@@ -1,69 +1,66 @@
-// jQuery to handle header sticking on screen on scroll
 jQuery(document).ready(function($){
-	$(window).scroll(function(){
-		if($(this).scrollTop() >= 100){
-			$('header').addClass('fixed');
-		}else{
-			$('header').removeClass('fixed');
-		}
-	});
+    $(window).scroll(function(){
+        if($(this).scrollTop() >= 100){
+            $('header').addClass('fixed');
+        }else{
+            $('header').removeClass('fixed');
+        }
+    });
 
+    var lightbox = $('.port-items a').simpleLightbox({ history: false });
 
-	var lightbox = $('.port-items a').simpleLightbox({ history: false });
+    var createdate = new Date();
+    var getyear = createdate.getFullYear();
+    var getyeardiv = document.getElementById('year');
+    getyeardiv.innerHTML = getyear;
 
-	
-	var createdate = new Date();
-	var getyear = createdate.getFullYear();
-	var getyeardiv = document.getElementById('year');
-	getyeardiv.innerHTML = getyear;
+    $('a.showmenu').click(function(){
+        $('nav').fadeIn();
+        $('body').addClass('disablescroll');
+        $('a.hidemenu').show();
+    });
 
+    $('a.hidemenu').click(function(){
+        $('nav').fadeOut();
+        $('body').removeClass('disablescroll');
+        $(this).hide();
+    });
 
-	$('a.showmenu').click(function(){
-		$('nav').fadeIn();
-		$('body').addClass('disablescroll');
-		$('a.hidemenu').show();
-	});
-	$('a.hidemenu').click(function(){
-		$('nav').fadeOut();
-		$('body').removeClass('disablescroll');
-		$(this).hide();
-	});
+    $(window).on("resize", function() {
+        // Ensure menu visibility on resize
+        const viewportWidth = $(window).width();
+        if (viewportWidth >= 1200) {
+            $('nav').show(); // Always show the menu on larger screens
+            $('a.hidemenu').hide();
+            $('a.showmenu').hide();
+            $('body').removeClass('disablescroll');
+        } else {
+            $('nav').hide(); // Default to hidden for smaller screens
+            $('a.showmenu').show();
+        }
+    }).trigger("resize"); // Trigger resize on page load to ensure correct initial state
 
-	var viewportWidth = $(window).width();
-    if (viewportWidth < 1200) {
-    	$("nav a").click(function() {
-			$("nav").fadeOut();
-			$(".hidemenu").hide();
-			$(".showmenu").show();
-			$('body').removeClass('disablescroll');
-		});
-    }
+    $(window).on("scroll", function() {
+        const $header = $("header");
+        const $menuLinks = $(".menu nav ul li a");
+        const $activeLink = $(".menu nav ul li.active a");
 
-    
-    $(function() {
-		$('a.scroll').click(function() {
-			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-				var target = $(this.hash);
-				target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-				if (target.length) {
-					$('html,body').animate({scrollTop: target.offset().top - 120}, 1200);
-					return false;
-				}
-			}
-		});
-	});
-});
-
-// JavaScript to handle font color change on scroll
-window.addEventListener("scroll", function() {
-    const header = document.querySelector("header");
-    const menuLinks = document.querySelectorAll(".menu nav ul li a");
-    
-    if (window.scrollY > 50) {
-      header.classList.add("scrolled");
-      menuLinks.forEach(link => link.style.color = "#000"); // Set menu links to black on scroll
-    } else {
-      header.classList.remove("scrolled");
-      menuLinks.forEach(link => link.style.color = "#fff"); // Set menu links to white at the top
-    }
+        if ($(this).scrollTop() > 50) {
+            $header.addClass("scrolled");
+            $menuLinks.css("color", "#000");
+            if ($activeLink.length) {
+                $activeLink.css("color", "#fff");
+            }
+        } else {
+            $header.removeClass("scrolled");
+            if ($(window).width() < 1200) {
+                $menuLinks.css("color", "#000");
+            } else {
+                $menuLinks.css("color", "#fff");
+            }
+            if ($activeLink.length) {
+                $activeLink.css("color", "#fff");
+            }
+        }
+    });
 });
